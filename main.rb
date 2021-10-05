03ed2a737671c3a70b802b490f5d8951d1b175a9
Trizny Larasati <32448006+triznylaras@users.noreply.github.com>
2021-10-05 16:37:01 +0000

    create method for counting words from the text that matches the given string array

--- main.rb
+++ main.rb
@@ -1,12 +1,25 @@
-def substrings(word, dictionary)
-  word.downcase!
-  if dictionary.include ? word
-    puts "Yes"
-  else puts "NO"
+def substrings(text, dictionary)
+  #case insensitive
+  text.downcase!
+  #substring array from the text
+  substring = text.split(" ")
+
+  #new hash for the substring and its count
+  frequencies = Hash.new(0)
+
+  substring.each do |string|
+    word = string.gsub(/[^0-9A-Za-z]/, '')  #del special char
+    #add to frequencies hash if a word matches dictionary
+    if dictionary.include? word
+      frequencies[word] += 1
+    else
+      next
+    end
   end
+  print frequencies
 end
 
-puts "Please enter your word:"
-word = gets.chomp
+puts "Please enter your text:"
+text = gets.chomp
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings(word, dictionary)
+substrings(text, dictionary)
